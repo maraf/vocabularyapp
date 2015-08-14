eab828f60c127b2a2dfcf3fecefd0c7e2a9ca4a7
maraf <mara@neptuo.com>
2015-08-14 16:53:55 -0700

    Update layout in browse.

--- app/src/main/java/com/neptuo/vocabularyapp/ui/BrowseActivity.java
+++ app/src/main/java/com/neptuo/vocabularyapp/ui/BrowseActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.FrameLayout;
